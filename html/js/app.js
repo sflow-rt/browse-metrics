9a8144b5a2bd5d5931a1fc22b5b88269b4fe6cba
pphaal <peter.phaal@inmon.com>
2019-10-11 14:28:16 -0700

    Reflect settings in URL

--- html/js/app.js
+++ html/js/app.js
@@ -1,8 +1,54 @@
 $(function() {
-  var selectedAgent = window.sessionStorage.getItem('mb_agent') || 'ALL';
-  var selectedMetric = window.sessionStorage.getItem('mb_metric');
-  var selectedDatasource = window.sessionStorage.getItem('mb_datasource') || 'ALL';
-  var selectedAggregation = window.sessionStorage.getItem('mb_aggregation') || 'max';
+  var defaults = {
+    agent: 'ALL',
+    datasource: 'ALL',
+    aggregation: 'max'
+  };
+
+  var state = {};
+  $.extend(state,defaults);
+
+  function createQuery(params) {
+    var query, key, value;
+    for(key in params) {
+      value = params[key];
+      if(value === defaults[key]) continue;
+      if(query) query += '&';
+      else query = '';
+      query += encodeURIComponent(key)+'='+encodeURIComponent(value);
+    }
+    return query;
+  }
+
+  function getState(key, defVal) {
+    return window.sessionStorage.getItem('mb_'+key) || state[key] || defVal;
+  }
+
+  function setState(key, val, showQuery) {
+    state[key] = val;
+    window.sessionStorage.setItem('mb_'+key, val);
+    if(showQuery) {
+      var query = createQuery(state);
+      window.history.replaceState({},'',query ? '?' + query : './');
+    }
+  }
+
+  function setQueryParams(query) {
+    var vars = query.split('&');
+    var params = {};
+    for(var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+      if(pair.length === 2) setState(decodeURIComponent(pair[0]), decodeURIComponent(pair[1]),false);
+    }
+  }
+
+  var search = window.location.search;
+  if(search) setQueryParams(search.substring(1));  
+ 
+  var selectedAgent = getState('agent');
+  var selectedMetric = getState('metric');
+  var selectedDatasource = getState('datasource');
+  var selectedAggregation = getState('aggregation');
 
   var names = {};
   var maxPoints = 5 * 60;
@@ -60,7 +106,10 @@ $(function() {
         var metricSelect = $('#metric');
         var dsSelect = $('#datasource');
         var i, metricsList = Object.keys(metrics).sort();
-        if(!metrics[selectedMetric]) selectedMetric = metricsList[0];
+        if(!metrics[selectedMetric]) {
+          selectedMetric = metricsList[0];
+          setState('metric',selectedMetric,true);
+        }
         metricSelect.empty();
         for(i = 0; i < metricsList.length; i++) {
           metricSelect.append('<option value="'+metricsList[i]+'"' + (selectedMetric == metricsList[i] ? ' selected' : '') + '>'+metricsList[i]+'</option>');
@@ -68,7 +117,7 @@ $(function() {
         dsSelect.empty();
         dsSelect.append('<option value="ALL" selected>ALL</option>');
         selectedDatasource = 'ALL';
-        window.sessionStorage.setItem('mb_datasource',selectedDatasource);
+        setState('datasource',selectedDatasource,true);
         resetChart();
       });
     } else {
@@ -88,7 +137,10 @@ $(function() {
           dsList.push(ds); 
         }
         nameList = Object.keys(names).sort();
-        if(!names[selectedMetric]) selectedMetric = nameList[0];
+        if(!names[selectedMetric]) {
+          selectedMetric = nameList[0];
+          setState('metric',selectedMetric,true);
+        }
         metricSelect.empty();
         for(i = 0; i < nameList.length; i++) {
           metricSelect.append('<option value="'+nameList[i]+'"' + (selectedMetric == nameList[i] ? ' selected' : '') + '>'+nameList[i]+'</option>');
@@ -105,12 +157,12 @@ $(function() {
   }
   $('#agent').change(function(evt) {
     selectedAgent = $('#agent').children('option:selected').val();
-    window.sessionStorage.setItem('mb_agent',selectedAgent);
+    setState('agent',selectedAgent,true);
     updateMetrics();
   });
   $('#metric').change(function(evt) {
     selectedMetric = $('#metric').children('option:selected').val();
-    window.sessionStorage.setItem('mb_metric',selectedMetric);
+    setState('metric',selectedMetric,true);
     var dsSelect = $('#datasource');
     var i, dsList = names[selectedMetric];
     dsSelect.empty();
@@ -127,13 +179,13 @@ $(function() {
   });
   $('#datasource').change(function(evt) {
     selectedDatasource = $('#datasource').children('option:selected').val();
-    window.sessionStorage.setItem('mb_datasource',selectedDatasource);
+    setState('datasource',selectedDatasource,true);
     resetChart();
   });
   $('#aggregation option[value="'+selectedAggregation+'"]').prop('selected',true);
   $('#aggregation').change(function(evt) {
     selectedAggregation = $('#aggregation').children('option:selected').val();
-    window.sessionStorage.setItem('mb_aggregation',selectedAggregation);
+    setState('aggregation',selectedAggregation,true);
     resetChart();
   });
   $.get('../../../agents/json', function(agents) {
